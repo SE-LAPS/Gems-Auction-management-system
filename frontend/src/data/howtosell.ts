68b21d63d14b8a2abdf9463b1d1bf51b894ac8c4
Lahiru Senavirathna <lahiruprasangika2018@gmail.com>
2024-10-28 17:05:58 +0530

    Create howtosell.ts

---
+++ frontend/src/data/howtosell.ts
@@ -0,0 +1,32 @@
+export const howToSell = [
+  {
+    stepNo: 1,
+    stepType: 'Registration',
+    desc: 'Clearly state your pricing structure, payment terms and and any additional charges. Specifying when & payment invoices will be issued, as well as your accepted.',
+    link: '/signup',
+  },
+  {
+    stepNo: 2,
+    stepType: 'Create the store',
+    desc: 'Set up your store by providing essential information, including your store name, contact details, and preferred categories for your items. Customize your store’s appearance to reflect your brand, upload a logo, and specify shipping options and delivery timelines.',
+    link: '/create-store',
+  },
+  {
+    stepNo: 3,
+    stepType: 'Listing the Item',
+    desc: 'Clearly state your pricing structure, payment terms and and any additional charges. Specifying when & payment invoices will be issued, as well as your accepted.',
+    link: '/add-art',
+  },
+  {
+    stepNo: 4,
+    stepType: 'Managing the Auction',
+    desc: 'Clearly state your pricing structure, payment terms and and any additional charges. Specifying when & payment invoices will be issued, as well as your accepted.',
+    link: '',
+  },
+  {
+    stepNo: 5,
+    stepType: 'Closing the Auction',
+    desc: 'Clearly state your pricing structure, payment terms and and any additional charges. Specifying when & payment invoices will be issued, as well as your accepted.',
+    link: '',
+  },
+];
