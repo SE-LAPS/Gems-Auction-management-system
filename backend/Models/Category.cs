5766493f0907d19eb227c4476b36f8facbeb1d23
Lahiru Senavirathna <lahiruprasangika2018@gmail.com>
2024-10-28 17:08:47 +0530

    Create Category.cs

---
+++ backend/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace backend.Models
+{
+    [Table("Category")]
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string CategoryName {  get; set; } = string.Empty;
+        [Required]
+        public string Image {  get; set; } = string.Empty;
+        [Required]
+        public string Icon {  get; set; } = string.Empty;
+        public List<Art> Arts { get; set; } = new List<Art>();
+    }
+}
