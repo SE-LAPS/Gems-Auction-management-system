8ea5954fca2539760347ec49a7acb9ce63702602
Lahiru Senavirathna <lahiruprasangika2018@gmail.com>
2024-10-28 16:57:21 +0530

    Create BidDto.cs

---
+++ backend/Dto/Bid/BidDto.cs
@@ -0,0 +1,16 @@
+﻿using backend.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace backend.Dto.Bid
+{
+    public class BidDto
+    {
+        public int Id { get; set; }
+        public decimal Amount { get; set; }
+        public string UserId { get; set; } = string.Empty;
+        public int AuctionId { get; set; }
+        public string PaymentIntentId { get; set; } = string.Empty;
+        public DateTime BidDate { get; set; } = DateTime.Now;
+    }
+}
