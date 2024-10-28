9ec93571d4117923509cf4d7b0b489143b9c01a8
Lahiru Senavirathna <lahiruprasangika2018@gmail.com>
2024-10-28 16:57:24 +0530

    Create CapturePaymentDto.cs

---
+++ backend/Dto/Bid/CapturePaymentDto.cs
@@ -0,0 +1,9 @@
+﻿namespace backend.Dto.Bid
+{
+    public class CapturePaymentDto
+    {
+        public string PaymentIntentId { get; set; } = string.Empty;
+        public int AuctionId { get; set; }
+        public string UserId { get; set; } = string.Empty;
+    }
+}
