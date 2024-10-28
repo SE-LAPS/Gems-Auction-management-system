3bec5bc6bc407babd9e0a2718fda82331e10e130
Lahiru Senavirathna <lahiruprasangika2018@gmail.com>
2024-10-28 17:07:40 +0530

    Create IAuctionRepository.cs

---
+++ backend/Interfaces/IAuctionRepository.cs
@@ -0,0 +1,19 @@
+﻿using backend.Helpers;
+using backend.Models;
+
+namespace backend.Interfaces
+{
+    public interface IAuctionRepository
+    {
+        Task<List<Auction>> GetAllAsync();
+        Task<Auction?> GetByIdAsync(int id);
+        Task<Auction?> UpdateAsync(int id, Auction auctionModel);
+        Task<Auction> CreateAsync(Auction auctionModel);
+        Task<Auction?> DeleteAsync(int id);
+        Task<Auction?> GetByArtId(int id);
+        Task CheckAndUpdateStatus(Auction auction);
+        Task<List<Auction>> GetByUserAsync(string userId, QueryObject? queryObject = null);
+        Task<List<Auction>> GetLatestAsync(int? limit = null);
+
+    }
+}
