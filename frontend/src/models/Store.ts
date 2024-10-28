a8c11782a7b2a0125c06f582db89ccde53b4732e
Lahiru Senavirathna <lahiruprasangika2018@gmail.com>
2024-10-28 17:06:29 +0530

    Create Store.ts

---
+++ frontend/src/models/Store.ts
@@ -0,0 +1,18 @@
+export type StorePost = {
+  Name: string;
+  Email: string;
+  PhoneNumber: string;
+  Address: string;
+  CoverPhoto: string;
+  ProfilePhoto: string;
+};
+
+export type StoreGet = {
+  id: number;
+  name: string;
+  email: string;
+  phoneNumber: string;
+  address: string;
+  coverPhoto: string;
+  profilePhoto: string;
+};
