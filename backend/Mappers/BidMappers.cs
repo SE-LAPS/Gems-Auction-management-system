51dab35510845b84478a42906d06ddb3e70569de
Lahiru Senavirathna <lahiruprasangika2018@gmail.com>
2024-10-28 17:07:56 +0530

    Create BidMappers.cs

---
+++ backend/Mappers/BidMappers.cs
@@ -0,0 +1,34 @@
+﻿using backend.Models;
+using backend.Dto.Bid;
+using Stripe;
+
+namespace backend.Mappers
+{
+    public static class BidMappers
+    {
+        public static BidDto ToBidDto(this Bid bidModel) 
+        {
+            return new BidDto
+            {
+                Id = bidModel.Id,
+                Amount = bidModel.Amount,
+                UserId = bidModel.UserId,
+                AuctionId = bidModel.AuctionId,
+                PaymentIntentId = bidModel.PaymentIntentId,
+                BidDate = bidModel.BidDate,
+            };
+        
+        }
+
+        public static Bid ToPlaceBidDto(this PlaceBidDto bidDto, string userId, string paymentIntentId) 
+        {
+            return new Bid
+            {
+                Amount = bidDto.Amount,
+                UserId = userId,
+                AuctionId = bidDto.AuctionId,
+                PaymentIntentId =  paymentIntentId
+    };
+        }
+    }
+}
