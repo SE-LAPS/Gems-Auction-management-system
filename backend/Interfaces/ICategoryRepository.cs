43647e22b73486acf658d8a8a78d83bc6d6930df
Lahiru Senavirathna <lahiruprasangika2018@gmail.com>
2024-10-28 17:07:46 +0530

    Create ICategoryRepository.cs

---
+++ backend/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using backend.Models;
+
+namespace backend.Interfaces
+{
+    public interface ICategoryRepository
+    {
+        Task<List<Category>> GetAllAsync();
+        Task<Category?> GetByIdAsync(int id);
+        Task<Category?> UpdateAsync(int id, Category categoryModel);
+        Task<Category> CreateAsync(Category categoryModel);
+        Task<Category?> DeleteAsync(int id);
+    }
+}
