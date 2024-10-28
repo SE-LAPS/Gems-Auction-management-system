2998919106a9bc951f89cb144742a9bae280d270
Lahiru Senavirathna <lahiruprasangika2018@gmail.com>
2024-10-28 17:07:33 +0530

    Create ClaimsExtensions.cs

---
+++ backend/Extensions/ClaimsExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Claims;
+
+namespace backend.Extensions
+{
+    public static class ClaimsExtensions
+    {
+        public static string GetUsername(this ClaimsPrincipal user) {
+            if (user == null)
+            {
+                throw new ArgumentNullException(nameof(user), "ClaimsPrincipal cannot be null");
+            }
+
+            var nameClaim = user.Claims.SingleOrDefault(x => x.Type.Equals("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/givenname"));
+
+            return nameClaim?.Value;
+        }
+    }
+}
