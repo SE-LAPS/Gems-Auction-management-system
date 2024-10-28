b5bf28b80851d4234b605bf63f6d43650e8a6571
Lahiru Senavirathna <lahiruprasangika2018@gmail.com>
2024-10-28 17:07:15 +0530

    Create UpdateCategoryDto.cs

---
+++ backend/Dto/Category/UpdateCategoryDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace backend.Dto.Category
+{
+    public class UpdateCategoryDto
+    {
+        [Required]
+        public string CategoryName = string.Empty;
+        [Required]
+        public string Image = string.Empty;
+        [Required]
+        public string Icon = string.Empty;
+    }
+}
