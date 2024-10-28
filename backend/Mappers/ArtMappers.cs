7c794787a5d66fa1693c0b58ba6389fd12ec71dd
Lahiru Senavirathna <lahiruprasangika2018@gmail.com>
2024-10-28 17:07:54 +0530

    Create ArtMappers.cs

---
+++ backend/Mappers/ArtMappers.cs
@@ -0,0 +1,71 @@
+﻿using backend.Dto.Art;
+using backend.Models;
+
+namespace backend.Mappers
+{
+    public static class ArtMappers
+    {
+        public static ArtDto ToArtDto(this Art artModel)
+        {
+            return new ArtDto
+            {
+                Id = artModel.Id,
+                Lot = artModel.Lot,
+                Title = artModel.Title,
+                Image = artModel.Image,
+                CurrentMarketPrice = artModel.CurrentMarketPrice,
+                Condition = artModel.Condition,
+                isFramed = artModel.isFramed,
+                Height = artModel.Height,
+                Width = artModel.Width,
+                CreatedOn = artModel.CreatedOn,
+                StoreId = artModel.StoreId,
+                CategoryId = artModel.CategoryId
+            };
+        }
+
+        public static Art ToArtFromCreate(this CreateArtRequestDto artDto, int storeId)
+        {
+            string uniqueLot = GenerateUniqueLot();
+
+            return new Art
+            {
+                Lot = uniqueLot,
+                Title = artDto.Title,
+                Image = artDto.Image,
+                CurrentMarketPrice = artDto.CurrentMarketPrice,
+                Condition = artDto.Condition,
+                isFramed = artDto.isFramed,
+                Height = artDto.Height,
+                Width = artDto.Width,
+                StoreId = storeId,
+                CategoryId = artDto.CategoryId,
+            };
+        }
+
+        private static string GenerateUniqueLot()
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            var random = new Random();
+            return new string(Enumerable.Repeat(chars, 12)
+                .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+
+        public static Art ToArtFromUpdate(this UpdateArtRequestDto artDto,int id)
+        {
+            return new Art
+            {
+                Title = artDto.Title,
+                Image = artDto.Image,
+                CurrentMarketPrice = artDto.CurrentMarketPrice,
+                Condition = artDto.Condition,
+                isFramed = artDto.isFramed,
+                Height = artDto.Height,
+                Width = artDto.Width,
+                StoreId = artDto.StoreId,
+                CategoryId = artDto.CategoryId
+            };
+        }
+    }
+}
