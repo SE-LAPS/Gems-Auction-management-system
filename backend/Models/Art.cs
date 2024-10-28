4d34d3286576b237ac61139399b43f3d0144c340
Lahiru Senavirathna <lahiruprasangika2018@gmail.com>
2024-10-28 17:08:33 +0530

    Create Art.cs

---
+++ backend/Models/Art.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace backend.Models
+{
+    [Table("Art")]
+    public class Art
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Lot {  get; set; } = string.Empty;
+        [Required]
+        public string Title { get; set; } = string.Empty;
+        [Required]
+        public string Image {  get; set; } = string.Empty;
+        [Required]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal CurrentMarketPrice {  get; set; }
+        [Required]
+        public string Condition { get; set; } = string.Empty;
+        [Required]
+        public bool isFramed { get; set; }
+        [Required]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Height { get; set; }
+        [Required]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Width { get; set; }
+        public DateTime CreatedOn { get; set; } = DateTime.Now;
+        [Required]
+        [ForeignKey("Store")]
+        public int StoreId { get; set; }
+        public Store Store { get; set; }
+        [Required]
+        [ForeignKey("Category")]
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+        public Auction Auction { get; set; }
+    }
+}
