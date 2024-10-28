10544cdcc81ccdc63f4acee171ee751b12adafb3
Lahiru Senavirathna <lahiruprasangika2018@gmail.com>
2024-10-28 17:08:23 +0530

    Create Auction.cs

---
+++ backend/Models/Auction.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace backend.Models
+{
+    [Table("Auction")]
+    public class Auction
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Status { get; set; } = string.Empty;
+        [Required]
+        public DateTime StartDate { get; set; }
+        [Required]
+        public DateTime EndDate { get; set; }
+        [Required]
+        [ForeignKey("Art")]
+        public int ArtId { get; set; }
+        public Art Art { get; set; }
+        [Required]
+        [ForeignKey("ApplicationUser")]
+        public string UserId { get; set; } = string.Empty;
+        public User User { get; set; }
+        public List<Bid> Bids { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+
+    }
+}
