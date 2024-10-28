3181359f56da840ec602e93d92fad941bef86f36
Lahiru Senavirathna <lahiruprasangika2018@gmail.com>
2024-10-28 16:57:12 +0530

    Create UpdateAuctionDto.cs

---
+++ backend/Dto/Auction/UpdateAuctionDto.cs
@@ -0,0 +1,17 @@
+﻿using backend.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace backend.Dto.Auction
+{
+    public class UpdateAuctionDto
+    {
+        [Required]
+        public string Status { get; set; } = string.Empty;
+        [Required]
+        public DateTime StartDate { get; set; }
+        [Required]
+        public DateTime EndDate { get; set; } = DateTime.MinValue;
+        [Required]
+        public int ArtId { get; set; }
+    }
+}
