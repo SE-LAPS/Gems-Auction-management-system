52db3aabc75fb59a4b8787cb0a315e486f90d974
Lahiru Senavirathna <lahiruprasangika2018@gmail.com>
2024-10-28 17:05:51 +0530

    Create buyerDashboard.ts

---
+++ frontend/src/data/buyerDashboard.ts
@@ -0,0 +1,29 @@
+import {
+  RiDashboardHorizontalLine,
+  RiAuctionLine,
+  RiWalletLine,
+  RiLogoutBoxRLine,
+} from '@remixicon/react';
+
+export const buyerDashboard = [
+  {
+    item: 'Dashboard',
+    component: 'Dashboard',
+    icon: RiDashboardHorizontalLine,
+  },
+  {
+    item: 'My Auction',
+    component: 'MyAuction',
+    icon: RiAuctionLine,
+  },
+  {
+    item: 'Payment',
+    component: 'Payment',
+    icon: RiWalletLine,
+  },
+  {
+    item: 'Logout',
+    component: 'Logout',
+    icon: RiLogoutBoxRLine,
+  },
+];
