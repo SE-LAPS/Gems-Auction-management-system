067a72e01b0b5a0e49f38208d47b744157d43e95
Lahiru Senavirathna <lahiruprasangika2018@gmail.com>
2024-10-28 17:07:38 +0530

    Create IBidRepository.cs

---
+++ backend/Interfaces/IBidRepository.cs
@@ -0,0 +1,12 @@
+﻿using backend.Models;
+
+namespace backend.Interfaces
+{
+    public interface IBidRepository
+    {
+        Task <List<Bid>> GetAllAsync();
+        Task <List<Bid>> GetByAuctionAsync(int id);
+        Task<List<Bid>> GetByUserAsync(string userId);
+        Task<Bid> PlaceBidAsync(Bid bidModel);
+    }
+}
