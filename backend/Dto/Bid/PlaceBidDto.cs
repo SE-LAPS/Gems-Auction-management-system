29dadd9837fd1c59927aef422728d272e8f78534
Lahiru Senavirathna <lahiruprasangika2018@gmail.com>
2024-10-28 16:57:26 +0530

    Create PlaceBidDto.cs

---
+++ backend/Dto/Bid/PlaceBidDto.cs
@@ -0,0 +1,17 @@
+﻿using backend.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace backend.Dto.Bid
+{
+    public class PlaceBidDto
+    {
+        [Required]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Amount { get; set; }
+        [Required]
+        [ForeignKey("Auction")]
+        public int AuctionId { get; set; }
+
+    }
+}
