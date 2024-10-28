51fe74d46cbf3791f36b4198e2dafb4f5029bd75
Lahiru Senavirathna <lahiruprasangika2018@gmail.com>
2024-10-28 16:58:38 +0530

    Create CreateStoreDto.cs

---
+++ backend/Dto/Store/CreateStoreDto.cs
@@ -0,0 +1,24 @@
+
+using System.ComponentModel.DataAnnotations;
+
+namespace backend.Dto.Store
+{
+    public class CreateStoreDto
+    {
+        [Required]
+        public string Name { get; set; } = string.Empty;
+        [Required]
+        public string Email { get; set; } = string.Empty;
+        [Required]
+        public string PhoneNumber { get; set; } = string.Empty;
+        [Required]
+        public string Address { get; set; } = string.Empty;
+        [Required]
+        public string CoverPhoto {  get; set; } = string.Empty;
+        [Required]
+        public string ProfilePhoto {  get; set; } = string.Empty;
+
+      
+
+    }
+}
