7f88e1fd6dbf46c306b4f1b3c5cee9dfd7badf52
Lahiru Senavirathna <lahiruprasangika2018@gmail.com>
2024-10-28 17:09:14 +0530

    Create BidRepository.cs

---
+++ backend/Repository/BidRepository.cs
@@ -0,0 +1,35 @@
+﻿using backend.Data;
+using backend.Interfaces;
+using backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Repository
+{
+    public class BidRepository: IBidRepository
+    {
+        private readonly ApplicationDBContext _context;
+        public BidRepository(ApplicationDBContext context) 
+        {
+            _context = context;
+        }
+
+        public async Task<List<Bid>> GetAllAsync()
+        {
+            return await _context.Bid.ToListAsync();
+        }
+        public async Task<List<Bid>> GetByAuctionAsync(int id)
+        {
+            return await _context.Bid.Where(x => x.AuctionId == id).OrderByDescending(a => a.BidDate).ToListAsync();
+        }
+        public async Task<List<Bid>> GetByUserAsync(string userId)
+        {
+            return await _context.Bid.Where(x => x.UserId == userId).ToListAsync();
+        }
+        public async Task<Bid> PlaceBidAsync(Bid bidModel)
+        {
+            await _context.Bid.AddAsync(bidModel);
+            await _context.SaveChangesAsync();
+            return bidModel;
+        }
+    }
+}
