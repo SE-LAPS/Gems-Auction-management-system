9eea06e38301094f13266401b5d286de07675450
Lahiru Senavirathna <lahiruprasangika2018@gmail.com>
2024-10-28 16:57:33 +0530

    Create CreateCategoryDto.cs

---
+++ backend/Dto/Category/CreateCategoryDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace backend.Dto.Category
+{
+    public class CreateCategoryDto
+    {
+        [Required]
+        public string CategoryName { get; set; } = string.Empty;
+        [Required]
+        public string Image { get; set; } = string.Empty;
+        [Required]
+        public string Icon {  get; set; } = string.Empty;
+    }
+}
