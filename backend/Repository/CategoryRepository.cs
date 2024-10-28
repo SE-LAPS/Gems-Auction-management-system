a3096eb8538a339d5940cee7dd5f015918fbe9c0
Lahiru Senavirathna <lahiruprasangika2018@gmail.com>
2024-10-28 17:08:59 +0530

    Create CategoryRepository.cs

---
+++ backend/Repository/CategoryRepository.cs
@@ -0,0 +1,64 @@
+﻿using backend.Data;
+using backend.Interfaces;
+using backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Repository
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly ApplicationDBContext _context;
+
+        public CategoryRepository(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Category>> GetAllAsync()
+        {
+            return await _context.Category.ToListAsync();
+        }
+        public async Task<Category> CreateAsync(Category categoryModel)
+        {
+            await _context.Category.AddAsync(categoryModel);
+            await _context.SaveChangesAsync();
+            return categoryModel;
+        }
+        public async Task<Category?> GetByIdAsync(int id)
+        {
+            return await _context.Category.FirstOrDefaultAsync(x => x.Id == id);
+
+        }
+        public async Task<Category?> UpdateAsync(int id, Category categoryModel)
+        {
+            var existingCategory = await _context.Category.FindAsync(id);
+
+            if (existingCategory == null)
+            {
+                return null;
+            }
+
+            existingCategory.CategoryName = categoryModel.CategoryName;
+            existingCategory.Image = categoryModel.Image;
+            existingCategory.Icon = categoryModel.Icon;
+
+            await _context.SaveChangesAsync();
+            return existingCategory;
+
+        }
+        public async Task<Category?> DeleteAsync(int id)
+        {
+            var categoryModel = await _context.Category.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (categoryModel == null)
+            {
+                return null;
+            }
+
+            _context.Category.Remove(categoryModel);
+            await _context.SaveChangesAsync();
+            return categoryModel;
+
+        }
+    }
+}
