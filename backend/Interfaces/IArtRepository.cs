219784b94722b260dc94a8ae6f75eb2ba16f72bc
Lahiru Senavirathna <lahiruprasangika2018@gmail.com>
2024-10-28 17:07:25 +0530

    Create IArtRepository.cs

---
+++ backend/Interfaces/IArtRepository.cs
@@ -0,0 +1,18 @@
+﻿using backend.Helpers;
+using backend.Models;
+
+namespace backend.Interfaces
+{
+    public interface IArtRepository
+    {
+        Task<List<Art>> GetAllAsync();
+        Task<Art?> GetByIdAsync(int id);
+        Task<List<Art?>> GetByCategoryAsync(int id);
+        Task<List<Art?>> GetByStoreAsync(int id, QueryObject? query = null);
+        Task<Art?> UpdateAsync(int id, Art artModel);
+        Task<Art> CreateAsync(Art artModel);
+        Task<Art?> DeleteAsync(int id);
+        Task<int> CountAsync(int id);
+        Task<Art?> UpdateCurrentMarketPriceAsync(int id, decimal amount);
+    }
+}
