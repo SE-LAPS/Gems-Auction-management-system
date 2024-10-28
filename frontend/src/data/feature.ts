dc2c3d69105065aab26fbf8a4559f9b550b3c311
Lahiru Senavirathna <lahiruprasangika2018@gmail.com>
2024-10-28 17:05:56 +0530

    Create feature.ts

---
+++ frontend/src/data/feature.ts
@@ -0,0 +1,29 @@
+import {
+  RiAuctionLine,
+  RiQuestionnaireLine,
+  RiSecurePaymentLine,
+  RiShakeHandsLine,
+} from '@remixicon/react';
+
+export const features = [
+  {
+    title: 'Discover the best deals',
+    des: 'Egestas libero. Aenean id lacin est. Mauris urn purus, docni.',
+    icon: RiShakeHandsLine,
+  },
+  {
+    title: 'Standout Auctions',
+    des: 'Egestas libero. Aenean id lacin est. Mauris urn purus, docni.',
+    icon: RiAuctionLine,
+  },
+  {
+    title: 'Pay safely',
+    des: 'Egestas libero. Aenean id lacin est. Mauris urn purus, docni.',
+    icon: RiSecurePaymentLine,
+  },
+  {
+    title: 'We’re here to help',
+    des: 'Egestas libero. Aenean id lacin est. Mauris urn purus, docni.',
+    icon: RiQuestionnaireLine,
+  },
+];
