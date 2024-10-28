58da5446fd6609228f8c400bb8f686175da3e7f3
Lahiru Senavirathna <lahiruprasangika2018@gmail.com>
2024-10-28 16:57:30 +0530

    Create CategoryDto.cs

---
+++ backend/Dto/Category/CategoryDto.cs
@@ -0,0 +1,10 @@
+﻿namespace backend.Dto.Category
+{
+    public class CategoryDto
+    {
+        public int Id { get; set; }
+        public string CategoryName { get; set; } = string.Empty;
+        public string Image { get; set; } = string.Empty;
+        public string Icon { get; set; } = string.Empty;
+    }
+}
