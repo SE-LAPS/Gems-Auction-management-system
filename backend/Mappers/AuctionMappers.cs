9c91e048cb98509d7f23c9f121a04f667cfdeab7
Lahiru Senavirathna <lahiruprasangika2018@gmail.com>
2024-10-28 17:08:20 +0530

    Create AuctionMappers.cs

---
+++ backend/Mappers/AuctionMappers.cs
@@ -0,0 +1,42 @@
+﻿using backend.Dto.Auction;
+using backend.Models;
+
+namespace backend.Mappers
+{
+    public static class AuctionMappers
+    {
+        public static AuctionDto ToAuctionDto(this Auction auctionModel)
+        {
+            return new AuctionDto
+            {
+                Id = auctionModel.Id,
+                Status = auctionModel.Status,
+                StartDate = auctionModel.StartDate,
+                EndDate = auctionModel.EndDate,
+                UserId = auctionModel.UserId,
+                ArtId = auctionModel.ArtId,
+                CreatedDate = auctionModel.CreatedDate,
+            };
+        }
+        public static Auction ToCreateAuctionDto(this CreateAuctionDto auctionDto, string userId)
+        {
+            return new Auction
+            {
+                StartDate = auctionDto.StartDate,
+                EndDate = auctionDto.EndDate,
+                UserId = userId,
+                ArtId = auctionDto.ArtId,
+            };
+        }
+        public static Auction ToUpdateAuctionDto(this UpdateAuctionDto auctionDto, int id)
+        {
+            return new Auction
+            {
+                Status = auctionDto.Status,
+                StartDate = auctionDto.StartDate,
+                EndDate = auctionDto.EndDate,
+                ArtId = auctionDto.ArtId
+            };
+        }
+    }
+}
