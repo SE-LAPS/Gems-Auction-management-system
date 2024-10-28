ea10351d5a4e8c05c3c6dd357f76af8559adecfb
Lahiru Senavirathna <lahiruprasangika2018@gmail.com>
2024-10-28 16:57:09 +0530

    Create ArtDto.cs

---
+++ backend/Dto/Art/ArtDto.cs
@@ -0,0 +1,19 @@
+﻿namespace backend.Dto.Art
+{
+    public class ArtDto
+    {
+        public int Id { get; set; }
+        public string Lot { get; set; } = string.Empty;
+        public string Title { get; set; } = string.Empty;
+        public string Image {  get; set; } = string.Empty;
+        public decimal CurrentMarketPrice { get; set; }
+        public string Condition { get; set; } = string.Empty;
+        public bool isFramed { get; set; }
+        public decimal Height { get; set; }
+        public decimal Width { get; set; }
+        public DateTime CreatedOn { get; set; } = DateTime.Now;
+        public int CategoryId { get; set; }
+        public int StoreId { get; set; }
+
+    }
+}
