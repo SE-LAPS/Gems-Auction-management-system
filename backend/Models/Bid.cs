dc451ca2afe4126f276dfe09ef13c25c4126bde8
Lahiru Senavirathna <lahiruprasangika2018@gmail.com>
2024-10-28 17:08:40 +0530

    Create Bid.cs

---
+++ backend/Models/Bid.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace backend.Models
+{
+    [Table("Bid")]
+    public class Bid
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Amount { get; set; }
+        [Required]
+        [ForeignKey("ApplicationUser")]
+        public string UserId { get; set; }
+        public User User { get; set; }
+        [Required]
+        [ForeignKey("Auction")]
+        public int AuctionId { get; set; }
+        public Auction Auction { get; set; }
+        public string PaymentIntentId { get; set; }
+        public DateTime BidDate { get; set; } = DateTime.Now;
+        public bool IsPaymentCaptured { get; internal set; } = false;
+    }
+}
