7e2fdfa0a6f74f4a9fd651c14ce2d56c75e6f5f4
Lahiru Senavirathna <lahiruprasangika2018@gmail.com>
2024-10-28 16:57:01 +0530

    Create UpdateArtRequestDto.cs

---
+++ backend/Dto/Art/UpdateArtRequestDto.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace backend.Dto.Art
+{
+    public class UpdateArtRequestDto
+    {
+        [Required]
+        [MinLength(5, ErrorMessage = "Title must be 5 characters")]
+        [MaxLength(280, ErrorMessage = "Title cannot be over 280 characters")]
+        public string Title { get; set; } = string.Empty;
+        public string Image { get; set; } = string.Empty;
+        [Required]
+        public decimal CurrentMarketPrice { get; set; }
+        [Required]
+        public string Condition { get; set; } = string.Empty;
+        [Required]
+        public bool isFramed { get; set; }
+        [Required]
+        public decimal Height { get; set; }
+        [Required]
+        public decimal Width { get; set; }
+        [Required]
+        public int StoreId { get; set; }
+        [Required]
+        public int CategoryId { get; set; }
+    }
+}
