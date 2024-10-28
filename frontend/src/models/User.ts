fb2fc07c80e9fa32d7d4e31b816d4f7fa26c6fe6
Lahiru Senavirathna <lahiruprasangika2018@gmail.com>
2024-10-28 17:06:31 +0530

    Create User.ts

---
+++ frontend/src/models/User.ts
@@ -0,0 +1,13 @@
+export type UserProfileToken = {
+  id: string;
+  userName: string;
+  email: string;
+  phoneNumber: string;
+  token: string;
+};
+
+export type UserProfile = {
+  userName: string;
+  email: string;
+  phoneNumber: string;
+};
