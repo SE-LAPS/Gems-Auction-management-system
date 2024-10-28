327fb7b7a85759107bfa6b4e70f1c713d085ce82
Lahiru Senavirathna <lahiruprasangika2018@gmail.com>
2024-10-28 17:07:28 +0530

    Create QueryObject.cs

---
+++ backend/Helpers/QueryObject.cs
@@ -0,0 +1,9 @@
+﻿namespace backend.Helpers
+{
+    public class QueryObject
+    {
+        public int PageNumber { get; set; } = 1;
+        public int PageSize { get; set; } = 10;
+        public string? Status { get; set; } = null;
+    }
+}
