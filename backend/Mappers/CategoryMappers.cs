8fded3fa147bee0a00b3e7aea1ed57e0506eb6fd
Lahiru Senavirathna <lahiruprasangika2018@gmail.com>
2024-10-29 17:01:07 +0530

    Create CategoryMappers.cs

---
+++ backend/Mappers/CategoryMappers.cs
@@ -0,0 +1,38 @@
+﻿using backend.Dto.Category;
+using backend.Models;
+
+namespace backend.Mappers
+{
+    public static class CategoryMappers
+    {
+        public static CategoryDto ToCategoryDto(this Category categoryModel)
+        {
+            return new CategoryDto
+            {
+                Id = categoryModel.Id,
+                CategoryName = categoryModel.CategoryName,
+                Image = categoryModel.Image,
+                Icon = categoryModel.Icon,
+            };
+        }
+
+        public static Category ToCategoryFromCreate(this CreateCategoryDto createCategoryDto) {
+            return new Category
+            {
+                CategoryName = createCategoryDto.CategoryName,
+                Image = createCategoryDto.Image,
+                Icon = createCategoryDto.Icon
+            };
+        }
+
+        public static Category ToCategoryFromUpdate(this UpdateCategoryDto categoryDto, int id)
+        {
+            return new Category
+            {
+                CategoryName = categoryDto.CategoryName,
+                Image = categoryDto.Image,
+                Icon = categoryDto.Icon
+            };
+        }
+    }
+}
