c89de2ebae4f91f02b3b9ef7e7cd298becbae320
Lahiru Senavirathna <lahiruprasangika2018@gmail.com>
2024-10-29 17:00:58 +0530

    Create User.cs

---
+++ backend/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace backend.Models
+{
+    [Table("ApplicationUser")]
+    public class User: IdentityUser
+    {
+        public virtual Store Store { get; set; }
+        public List<Art> Arts { get; set; } = new List<Art>();
+    }
+}
